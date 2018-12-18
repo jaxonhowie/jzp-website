20b7e9ffa4084a20bb66261db2b776bfd2f82ede
hongyi zheng <635072863@qq.com>
2018-12-18 14:06:49 +0800

    update

--- src/main/java/com/jzp/controller/api/tools/SteamApiController.java
+++ src/main/java/com/jzp/controller/api/tools/SteamApiController.java
@@ -3,16 +3,22 @@ package com.jzp.controller.api.tools;
 import com.alibaba.fastjson.JSONObject;
 import com.jzp.common.RspCode;
 import com.jzp.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Hongyi Zheng
  * @date 2018/12/17
  */
-@RestController("/steam")
+@Controller("/steam")
 public class SteamApiController extends BaseController {
 
-    public JSONObject getApiList(){
-        return result(RspCode.SUC);
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public String getApiList(Model model){
+        model.addAttribute("test", "test");
+        return "/menu/steam/apilist";
     }
 }
---
+++ src/main/java/com/jzp/controller/model/SteamApi.java
@@ -0,0 +1,12 @@
+package com.jzp.controller.model;
+
+/**
+ * @author Hongyi Zheng
+ * @date 2018/12/18
+ */
+public class SteamApi {
+    private String oid;
+    private String name;
+    private String params;
+
+}
